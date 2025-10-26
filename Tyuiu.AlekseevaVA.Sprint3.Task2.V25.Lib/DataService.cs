f155a2454cf3e25db96df92da49d7eb415baee1b
AlekseevaVA <alekseevavria@mail.ru>
2025-10-26 12:04:38 +0500

    Task2

---
+++ Tyuiu.AlekseevaVA.Sprint3.Task2.V25.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.AlekseevaVA.Sprint3.Task2.V25.Lib
+{
+    public class DataService : ISprint3Task2V25
+    {
+        public double GetSumSeries(int value, int startValue, int stopValue)
+        {
+            double SumSeries = 0;
+            do
+            {
+                SumSeries = SumSeries + Math.Pow(4 / Math.Pow(startValue, value), 2);
+                startValue++;
+            } while (startValue <= stopValue);
+            return Math.Round(SumSeries, 3);
+        }
+    }
+    
+}
---
+++ Tyuiu.AlekseevaVA.Sprint3.Task2.V25/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.AlekseevaVA.Sprint3.Task2.V25.Lib;
+
+namespace Tyuiu.AlekseevaVA.Sprint3.Task2.V25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнила: Алексеева В. А. | ИИПб-25-1 ";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла while                                              *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #25                                                             *");
+            Console.WriteLine("* Выполнила: Алексеева В. А. | ИИПб-25-1                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int value = 5;//n
+            int startValue = 1;//k
+            int stopValue = 13;
+            Console.WriteLine("X:" + value);
+            Console.WriteLine("старт шага:" + startValue);
+            Console.WriteLine("конец шага:" + stopValue);
+
+
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                           *");
+            Console.WriteLine("************************************************************************");
+
+            Console.WriteLine($"Ответ = " + ds.GetSumSeries(value, startValue, stopValue));
+
+            Console.ReadKey();
+        }
+    }
+}
