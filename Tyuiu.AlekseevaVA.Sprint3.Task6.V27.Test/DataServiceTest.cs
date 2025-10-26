6c9c186d59b6702e8e98c273d6d7e2f7d5c32170
AlekseevaVA <alekseevavria@mail.ru>
2025-10-26 20:07:14 +0500

    Task6

---
+++ Tyuiu.AlekseevaVA.Sprint3.Task6.V27.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.AlekseevaVA.Sprint3.Task6.V27.Lib;
+
+namespace Tyuiu.AlekseevaVA.Sprint3.Task6.V27.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int startValue = 14;
+            int stopValue = 20;
+
+
+            double res = ds.GetSumTheDivisors(startValue, stopValue);
+            double wait = 129;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.AlekseevaVA.Sprint3.Task6.V27/Program.cs
@@ -0,0 +1,47 @@
+﻿using Tyuiu.AlekseevaVA.Sprint3.Task6.V27.Lib;
+
+namespace Tyuiu.AlekseevaVA.Sprint3.Task6.V27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнила: Алексеева В. А. | ИИПб-25-1 ";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла while                                              *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #27                                                             *");
+            Console.WriteLine("* Выполнила: Алексеева В. А. | ИИПб-25-1                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = 14;
+            int stopValue = 20;
+
+            
+            Console.WriteLine("Начало отрезка = " + startValue);
+            Console.WriteLine("Конец отрезка = " + stopValue);
+            
+
+
+
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                           *");
+            Console.WriteLine("************************************************************************");
+
+            Console.WriteLine("Сумма делителей = " + ds.GetSumTheDivisors(startValue, stopValue));
+
+            Console.ReadKey();
+        }
+    }
+}
