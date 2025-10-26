eadc6f108588867c0441518e5c3b093dac410a4e
AlekseevaVA <alekseevavria@mail.ru>
2025-10-26 17:06:55 +0500

    Task4

---
+++ Tyuiu.AlekseevaVA.Sprint3.Task4.V12.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.AlekseevaVA.Sprint3.Task4.V12.Lib;
+
+namespace Tyuiu.AlekseevaVA.Sprint3.Task4.V12.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            double res = ds.Calculate(startValue, stopValue);
+
+            double wait = 467.59;
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
---
+++ Tyuiu.AlekseevaVA.Sprint3.Task4.V12/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.AlekseevaVA.Sprint3.Task4.V12.Lib;
+
+namespace Tyuiu.AlekseevaVA.Sprint3.Task4.V12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнила: Алексеева В. А. | ИИПб-25-1 ";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла while                                              *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #22                                                             *");
+            Console.WriteLine("* Выполнила: Алексеева В. А. | ИИПб-25-1                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            Console.WriteLine("Старт шага =" + startValue);
+            Console.WriteLine("Конец шага =" + stopValue);
+
+
+
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                           *");
+            Console.WriteLine("************************************************************************");
+
+            Console.WriteLine("Произведение ряда = " + ds.Calculate(startValue, stopValue));
+
+            Console.ReadKey();
+        }
+    }
+}
